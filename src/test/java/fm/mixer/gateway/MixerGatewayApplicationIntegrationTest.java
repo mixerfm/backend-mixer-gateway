935c0e26e4f651feaf8c46f5bd4e44a1aeb83034
skurelic <kurelic@sanjin.eu>
2024-05-15 09:52:13 +0200

    [ADD] #MMBE-50 - Add common test structures

---
+++ src/test/java/fm/mixer/gateway/MixerGatewayApplicationIntegrationTest.java
@@ -0,0 +1,16 @@
+package fm.mixer.gateway;
+
+import fm.mixer.gateway.test.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MixerGatewayApplicationIntegrationTest extends BaseIntegrationTest {
+
+    @Test
+    void contextLoads() {
+        final var contextLoaded = true;
+
+        assertThat(contextLoaded).isTrue();
+    }
+}
