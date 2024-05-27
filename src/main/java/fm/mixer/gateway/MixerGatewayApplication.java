72a52bb5781ac14307a66cbe570cc75511e76333
Nikola KNB <nikola@knb.io>
2024-05-27 21:20:26 +0200

    PoC deploy - merged changes, temporarily preserving docker config until we separate this out into CICD

--- src/main/java/fm/mixer/gateway/MixerGatewayApplication.java
+++ src/main/java/fm/mixer/gateway/MixerGatewayApplication.java
@@ -3,11 +3,9 @@ package fm.mixer.gateway;
 import fm.mixer.gateway.client.TestApiClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 @EnableFeignClients(clients = {TestApiClient.class})
 public class MixerGatewayApplication {
 
