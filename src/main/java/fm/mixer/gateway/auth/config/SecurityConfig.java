1a1b46ed0e5a7ea6a7422fa3168bd291ff335370
skurelic <kurelic@sanjin.eu>
2024-07-09 14:05:50 +0200

    [ADD] - #MMBE-279 - Add CORS settings and separate config for test cases

--- src/main/java/fm/mixer/gateway/auth/config/SecurityConfig.java
+++ src/main/java/fm/mixer/gateway/auth/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,69 +15,63 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
 
+import java.util.List;
 import java.util.stream.Stream;
 
+@Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
+@ConditionalOnProperty(prefix = "mixer-gateway.security", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class SecurityConfig {
 
-    @Configuration
-    @ConditionalOnProperty(prefix = "mixer-gateway.security", name = "enabled", havingValue = "false")
-    public static class DisabledSecurityConfig {
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+    private final AccessDeniedHandler accessDeniedHandler;
 
-        @Bean
-        public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-            // allow every requests
-            http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> {
+            authorize.requestMatchers(antPathRequestMatchers(
+                "/collections/*/reactions", "/comments/*/reactions", "/mixes/*/reactions", "/tracks/*/reactions",
+                "/users", "PUT /users/*", "DELETE /users/*",
+                "/users/*/follow", "/users/*/unfollow", "/users/*/remove-follower", "/users/*/reactions"
+            )).authenticated();
+            authorize.anyRequest().permitAll();
+        });
 
-            http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-            http.csrf(AbstractHttpConfigurer::disable);
+        http.oauth2ResourceServer(oauth -> oauth
+            .jwt(Customizer.withDefaults())
+            .accessDeniedHandler(accessDeniedHandler)
+            .authenticationEntryPoint(authenticationEntryPoint)
+        );
 
-            return http.build();
-        }
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.cors((corsConfigurer) -> corsConfigurer.configurationSource((cs) -> {
+            final var configuration = new CorsConfiguration();
+
+            configuration.setAllowedOriginPatterns(List.of("https://*.mixer.fm"));
+            configuration.setAllowedMethods(List.of(
+                HttpMethod.GET.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name(), HttpMethod.TRACE.name(),
+                HttpMethod.POST.name(), HttpMethod.PATCH.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name()
+            ));
+
+            return configuration;
+        }));
+
+        return http.build();
     }
 
-    @Configuration
-    @RequiredArgsConstructor
-    @ConditionalOnProperty(prefix = "mixer-gateway.security", name = "enabled", havingValue = "true", matchIfMissing = true)
-    public static class JwtSecurityConfig {
-
-        private final AuthenticationEntryPoint authenticationEntryPoint;
-        private final AccessDeniedHandler accessDeniedHandler;
-
-        @Bean
-        public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-            http.authorizeHttpRequests(authorize -> {
-                authorize.requestMatchers(antPathRequestMatchers(
-                    "/collections/*/reactions", "/comments/*/reactions", "/mixes/*/reactions", "/tracks/*/reactions",
-                    "/users", "PUT /users/*", "DELETE /users/*",
-                    "/users/*/follow", "/users/*/unfollow", "/users/*/remove-follower", "/users/*/reactions"
-                )).authenticated();
-                authorize.anyRequest().permitAll();
-            });
-
-            http.oauth2ResourceServer(oauth -> oauth
-                .jwt(Customizer.withDefaults())
-                .accessDeniedHandler(accessDeniedHandler)
-                .authenticationEntryPoint(authenticationEntryPoint)
-            );
-
-            http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-            http.csrf(AbstractHttpConfigurer::disable);
-
-            return http.build();
-        }
-
-        private static RequestMatcher[] antPathRequestMatchers(String... patterns) {
-            return Stream.of(patterns).map(p -> {
-                // Developer can define HTTP method in pattern, e.g.: "GET /endpoint"
-                // => using hardcoded array positions so errors can easily be detected on first run
-                final var pattern = p.split(" ");
-                if (pattern.length == 1) {
-                    return new AntPathRequestMatcher(p);
-                }
-                return new AntPathRequestMatcher(pattern[1], pattern[0]);
-            }).toArray(AntPathRequestMatcher[]::new);
-        }
+    private static RequestMatcher[] antPathRequestMatchers(String... patterns) {
+        return Stream.of(patterns).map(p -> {
+            // Developer can define HTTP method in pattern, e.g.: "GET /endpoint"
+            // => using hardcoded array positions so errors can easily be detected on first run
+            final var pattern = p.split(" ");
+            if (pattern.length == 1) {
+                return new AntPathRequestMatcher(p);
+            }
+            return new AntPathRequestMatcher(pattern[1], pattern[0]);
+        }).toArray(AntPathRequestMatcher[]::new);
     }
 }
