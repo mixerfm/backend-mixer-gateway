eaa94cbc206462d22dd07434bcef59f43c26840b
skurelic <kurelic@sanjin.eu>
2024-07-23 09:20:19 +0200

    [FIX] #MMBE-276 - Use handler instead of interceptor

--- src/main/java/fm/mixer/gateway/client/filter/ClientContextInterceptor.java
+++ src/main/java/fm/mixer/gateway/client/filter/ClientContextInterceptor.java
@@ -2,20 +2,16 @@ package fm.mixer.gateway.client.filter;
 
 import fm.mixer.gateway.client.holder.ClientContextHolder;
 import fm.mixer.gateway.client.model.ClientContext;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
+import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 @RequiredArgsConstructor
-public class ClientContextFilter extends OncePerRequestFilter {
+public class ClientContextInterceptor implements HandlerInterceptor {
 
     private static final String DEVICE_ID_HEADER = "X-Device-Id";
     private static final String CLIENT_COUNTRY_HEADER = "CF-IPCountry";
@@ -23,7 +19,7 @@ public class ClientContextFilter extends OncePerRequestFilter {
     private final ClientAccessFilter clientAccessFilter;
 
     @Override
-    protected void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
+    public boolean preHandle(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) {
         ClientContextHolder.set(new ClientContext(
             request.getHeader(DEVICE_ID_HEADER),
             request.getHeader(CLIENT_COUNTRY_HEADER)
@@ -31,6 +27,6 @@ public class ClientContextFilter extends OncePerRequestFilter {
 
         clientAccessFilter.checkClientAccess(request);
 
-        filterChain.doFilter(request, response);
+        return true;
     }
 }
